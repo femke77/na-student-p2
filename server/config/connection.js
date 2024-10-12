2eee0ad71f361b5d5631b91d3fd5ad63b55be85a
femke77 <megan.meyers.388@gmail.com>
2024-10-11 22:40:44 -0700

    trying to connect to render postgresql

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,10 @@ import "dotenv/config";
 
 const sequelize =
   process.env.NODE_ENV === "production"
-    ? new Sequelize(process.env.DATABASE_URL)
+    ? new Sequelize(process.env.DB_URL, {
+        dialect: "postgres",
+      })
+
     : new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,

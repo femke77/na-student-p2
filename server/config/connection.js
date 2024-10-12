b13e2937c81c6eb7e3a8fac7098be50961976ef8
femke77 <megan.meyers.388@gmail.com>
2024-10-11 22:37:24 -0700

    added postcss.config.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,9 @@ import "dotenv/config";
 
 const sequelize =
   process.env.NODE_ENV === "production"
-    ? new Sequelize(process.env.DATABASE_URL)
+    ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+    })
     : new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
